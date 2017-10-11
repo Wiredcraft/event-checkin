fffab8396803191058672c863401c1e4b5a65311
Paul Vollmer <paul.vollmer@protonmail.com>
2017-10-11 18:44:18 +0200

    Cleanup db setup script

--- deploy/setup.js
+++ deploy/setup.js
@@ -3,12 +3,9 @@ const ds = app.dataSources.eventcheckin
 
 let excludedModels = []
 const tables = Object.keys(app.models).filter(key => excludedModels.indexOf(key) === -1)
-
-console.log(tables);
 ds.automigrate(tables, function(err) {
-  console.log('yooo');
   if (err) {
-    console.log(err);
+    console.log('ERROR', err);
     throw err;
   }
   console.log('Loopback tables [' - tables - '] created in ', ds.adapter.name);
