0dd6231cbe313d963d90aa6c17bf9fe2ed673001
Paul Vollmer <paul.vollmer@protonmail.com>
2017-12-14 10:54:12 +0100

    Refactored mysql setup script

--- deploy/setup.js
+++ deploy/setup.js
@@ -1,13 +1,15 @@
-const app = require('../backend/server/server')
-const ds = app.dataSources.eventcheckin
+'use strict';
+
+const server = require('../backend/server/server')
+const mysql = server.dataSources.eventcheckin
 
 let excludedModels = []
-const tables = Object.keys(app.models).filter(key => excludedModels.indexOf(key) === -1)
-ds.automigrate(tables, function(err) {
+const tables = Object.keys(server.models).filter(key => excludedModels.indexOf(key) === -1)
+mysql.automigrate(tables, function(err) {
   if (err) {
     console.log('ERROR', err);
     throw err;
   }
-  console.log('Loopback tables [' - tables - '] created in ', ds.adapter.name);
-  ds.disconnect();
+  console.log('Loopback tables [' - tables - '] created in ', mysql.adapter.name);
+  mysql.disconnect();
 });
