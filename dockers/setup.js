64462186abf2f8ea66afc0748cfcc127fe71f3ff
Paul Vollmer <paul.vollmer@protonmail.com>
2018-02-22 11:21:52 +0100

    Docker setup.js fixed log

--- dockers/setup.js
+++ dockers/setup.js
@@ -1,14 +1,16 @@
-const path = require('path')
-const app = require(path.join(__dirname, '../backend/server/server'))
-const ds = app.dataSources.eventcheckin
+'use strict';
 
-let excludedModels = []
-const tables = Object.keys(app.models).filter(key => excludedModels.indexOf(key) === -1)
+const path = require('path');
+const app = require(path.join(__dirname, '../backend/server/server'));
+const ds = app.dataSources.eventcheckin;
+
+let excludedModels = [];
+const tables = Object.keys(app.models).filter(key => excludedModels.indexOf(key) === -1);
 ds.automigrate(tables, function(err) {
   if (err) {
     console.log('ERROR', err);
     throw err;
   }
-  console.log('Loopback tables [' - tables - '] created in ', ds.adapter.name);
+  console.log('Loopback tables [' + tables + '] created in ', ds.adapter.name);
   ds.disconnect();
 });
