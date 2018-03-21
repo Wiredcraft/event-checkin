9875f4f57f4f5597f460f1af7213fd59d7977f82
Patrick Mielke <patrick.mielke89@gmail.com>
2018-03-21 14:51:08 +0100

    Add to toLowerCase() to stringNormalizer #8

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,7 +1,13 @@
-// String normalizer
+// Utilities
 
+/* 
+  String normalizer for guest list search feature.
+
+  Normalizes strings by NFD and replaces Combining Diacritical Marks Unicode group characters.
+
+ */
 export const stringNormalizer = (str) => {
-	return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+	return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()
 }
 
 export default{
